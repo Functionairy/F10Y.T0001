43cfe0036c89058e691f24371fff9a55ae99645f
SURFEIT\David <david.coats@gmail.com>
2025-03-20 22:26:16 -0700

    Add the documentation-marker attribute class.

---
+++ source/F10Y.T0001/Code/Attributes/DocumentationsMarkerAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using F10Y.T0000;
+
+
+namespace F10Y.T0001
+{
+    /// <summary>
+    /// Attribute indicating a type is a documentations class. (Used for containing sets of documentation instances.)
+    /// </summary>
+    /// <remarks>
+    /// The marker attribute is useful for surveying assemblies and code, then building a catalogue of documentation classes.
+    /// </remarks>
+    [AttributeUsage(
+        AttributeTargets.Class, // Documentations should be in a static class (and nested static classes), so demand a class.
+        AllowMultiple = false, // A type either is or is not a documentations class. No need for multiple attributes.
+        Inherited = false // While attributes CAN be inherited, generally they are not. So force users to re-mark any inherited attribute types.
+    )]
+    [MarkerAttributeMarker]
+    public class DocumentationsMarkerAttribute : Attribute
+    {
+        /// <summary>
+        /// Allows specifying that a class is *not* a documentation class, even if it is marked with the documentations marker attribute.
+        /// This is useful for marking types that end up canonical documentation class code file locations, but are not actually documentation classes for surveying.
+        /// </summary>
+        public bool Is_Documentation { get; }
+
+
+        public DocumentationsMarkerAttribute(
+            bool is_Documentation = true)
+        {
+            this.Is_Documentation = is_Documentation;
+        }
+    }
+}
